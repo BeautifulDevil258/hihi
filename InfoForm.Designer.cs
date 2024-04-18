e383f60acb8f666dacf59024280d5508a525a83b
pivut <163645785+pivut@users.noreply.github.com>
2024-04-18 15:02:56 +0700

    Add files via upload

---
+++ InfoForm.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace dentistry6boy
+{
+    partial class InfoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DGV_Info = new System.Windows.Forms.DataGridView();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtTenhienthi = new System.Windows.Forms.TextBox();
+            this.txtRole = new System.Windows.Forms.TextBox();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnRef = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_Info)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGV_Info
+            // 
+            this.DGV_Info.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_Info.Location = new System.Drawing.Point(3, 305);
+            this.DGV_Info.Name = "DGV_Info";
+            this.DGV_Info.RowHeadersWidth = 51;
+            this.DGV_Info.RowTemplate.Height = 24;
+            this.DGV_Info.Size = new System.Drawing.Size(794, 139);
+            this.DGV_Info.TabIndex = 0;
+            this.DGV_Info.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGV_Info_CellContentClick);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(60, 89);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 22);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(265, 89);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(100, 22);
+            this.txtPass.TabIndex = 2;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(60, 174);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 22);
+            this.txtPhone.TabIndex = 3;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(265, 174);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 22);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // txtTenhienthi
+            // 
+            this.txtTenhienthi.Location = new System.Drawing.Point(480, 89);
+            this.txtTenhienthi.Name = "txtTenhienthi";
+            this.txtTenhienthi.Size = new System.Drawing.Size(100, 22);
+            this.txtTenhienthi.TabIndex = 5;
+            // 
+            // txtRole
+            // 
+            this.txtRole.Location = new System.Drawing.Point(480, 174);
+            this.txtRole.Name = "txtRole";
+            this.txtRole.Size = new System.Drawing.Size(100, 22);
+            this.txtRole.TabIndex = 6;
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(670, 185);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 7;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnRef
+            // 
+            this.btnRef.Location = new System.Drawing.Point(670, 125);
+            this.btnRef.Name = "btnRef";
+            this.btnRef.Size = new System.Drawing.Size(75, 23);
+            this.btnRef.TabIndex = 8;
+            this.btnRef.Text = "Refs";
+            this.btnRef.UseVisualStyleBackColor = true;
+            this.btnRef.Click += new System.EventHandler(this.btnRef_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(281, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(294, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(489, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Tên hiển thị";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(502, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Role";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(70, 155);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 16);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Số điện thoại";
+            // 
+            // InfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRef);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.txtRole);
+            this.Controls.Add(this.txtTenhienthi);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.DGV_Info);
+            this.Name = "InfoForm";
+            this.Text = "InfoForm";
+            this.Load += new System.EventHandler(this.InfoForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_Info)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGV_Info;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtTenhienthi;
+        private System.Windows.Forms.TextBox txtRole;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnRef;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ InfoForm.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static dentistry6boy.fAccountProfile;
+
+namespace dentistry6boy
+{
+    public partial class InfoForm : Form
+    {
+        SqlConnection sqlCon = null;
+        string strCon = ConfigurationManager.ConnectionStrings["QLNK"].ConnectionString.ToString();
+        int userID = SessionData.UserID;
+
+        public InfoForm()
+        {
+            InitializeComponent();
+        }
+        public void HienThi()
+        {
+            string sqlSELECT = "SELECT * FROM users WHERE UserID = @userID";
+
+            SqlCommand cmd = new SqlCommand(sqlSELECT, sqlCon);
+            cmd.Parameters.AddWithValue("@userID", userID);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            DGV_Info.DataSource = dt;
+        }
+        private void DGV_dv_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Kiểm tra xem có hàng nào được chọn không
+            if (e.RowIndex >= 0)
+            {
+                // Lấy dữ liệu từ hàng được chọn
+                DataGridViewRow row = DGV_Info.Rows[e.RowIndex];
+
+                // Hiển thị dữ liệu từ hàng được chọn vào các TextBox tương ứng
+                txtUsername.Text = row.Cells["Username"].Value.ToString();
+                txtPass.Text = row.Cells["Password"].Value.ToString();
+                txtPhone.Text = row.Cells["Phone"].Value.ToString();
+                txtEmail.Text = row.Cells["Email"].Value.ToString();
+                txtTenhienthi.Text = row.Cells["Tenhienthi"].Value.ToString();
+                txtRole.Text = row.Cells["Role"].Value.ToString();
+            }
+        }
+        private void DGV_Info_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void InfoForm_Load(object sender, EventArgs e)
+        {
+            if (sqlCon == null)
+            {
+                sqlCon = new SqlConnection(strCon);
+            }
+            if (sqlCon.State == ConnectionState.Closed)
+            {
+                sqlCon.Open();
+            }
+            HienThi();
+
+            // Gán sự kiện CellClick cho DataGridView
+            DGV_Info.CellClick += new DataGridViewCellEventHandler(DGV_dv_CellClick);
+
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            // Lấy giá trị từ các TextBox
+            string username = txtUsername.Text;
+            string password = txtPass.Text;
+            string phone = txtPhone.Text;
+            string email = txtEmail.Text;
+            string tenhienthi = txtTenhienthi.Text;
+            string role = txtRole.Text;
+
+            // Thực hiện cập nhật vào cơ sở dữ liệu
+            string sqlUpdate = "UPDATE users SET Username = @username, Password = @password, Phone = @phone, Email = @email, Tenhienthi = @tenhienthi, Role = @role WHERE UserID = @userID";
+            SqlCommand cmd = new SqlCommand(sqlUpdate, sqlCon);
+            cmd.Parameters.AddWithValue("@username", username);
+            cmd.Parameters.AddWithValue("@password", password);
+            cmd.Parameters.AddWithValue("@phone", phone);
+            cmd.Parameters.AddWithValue("@email", email);
+            cmd.Parameters.AddWithValue("@tenhienthi", tenhienthi);
+            cmd.Parameters.AddWithValue("@role", role);
+            cmd.Parameters.AddWithValue("@userID", userID); // userID là giá trị của UserID bạn muốn sửa
+
+            try
+            {
+                if (sqlCon.State != ConnectionState.Open)
+                {
+                    sqlCon.Open();
+                }
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Thông tin đã được cập nhật thành công.");
+                    HienThi();
+                    // Cập nhật lại giao diện hoặc làm bất kỳ xử lý nào khác sau khi cập nhật thành công
+                }
+                else
+                {
+                    MessageBox.Show("Không thể cập nhật thông tin.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+
+        private void btnRef_Click(object sender, EventArgs e)
+        {
+            HienThi();
+        }
+    }
+}
